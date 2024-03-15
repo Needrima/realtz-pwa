a772ef741779068308b7892a256ccf79e878c9df
needrima <oyebodeamirdeen@gmail.com>
2024-03-15 08:49:37 +0100

    landing page

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+        <h1 className='fw-bold mt-5 mb-3'>Create your <span className='text-default'>account</span></h1>
+
+        <div className='mb-3 text-muted'>Lorem ipsum dolor sit amet, consecturadipiscing</div>
+
+        
+    </div>
   )
 }
 
